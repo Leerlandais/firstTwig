e9a3b6e5310aecfca1dde6f224b7f9b448738786
leerlandais <leerlandais@gmail.com>
2024-06-30 08:23:47 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+// chemin vers Twig
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
+// chargement de l'autoload
+require_once '../vendor/autoload.php';
+
+// chemin vers les templates twig
+$loader = new FilesystemLoader('../view/');
+// création d'une instance de $twig
+$twig = new Environment($loader, [
+'cache' => false, // pas de cache en dev
+// 'cache' => '/path/to/compilation_cache', // chemin du cache pour la prod
+// activation du debug en dev
+'debug' => true,
+]);
+
+$array = [
+"un",
+"deux",
+"trois","quatre","cinq","six","sept",
+];
+
+// Appel de la vue de test
+echo $twig->render('test.html.twig', ['chiffres' => $array]);
